a16d023aa78f61b86d6dc51c134802e6e28b0cf5
Daler Sharifkulov <dsarifkulo021@gmail.com>
2025-02-18 15:09:16 +0500

    Update layout.tsx

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "VISTA TOUR",
+    title: "OKEYTOUR",
     description: "Путешествие по Узбекистану",
 };
 
